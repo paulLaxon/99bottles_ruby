185e97ff64230963f4b828eb521a9177e0cbec1c
Paul Laxon <pdl1963@gmail.com>
2021-12-28 15:20:16 -0500

    fixes - all tests passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,6 @@
 class Bottles
   def song
-    verses(99,0)
+    verses(99, 0)
   end
 
   def verses(upper, lower)
@@ -9,14 +9,33 @@ class Bottles
 
   def verse(number)
     bottle_number = BottleNumber.for(number)
-    "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of beer on the wall.\n"
+    "#{bottle_number} of beer on the wall, #{bottle_number} of beer.\n".capitalize +
+      "#{bottle_number.action}, #{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
 class BottleNumber
+  def self.for(number)
+    registry.find { |candidate| candidate.handles?(number) }.new(number)
+  end
+
+  def self.registry
+    @registry ||= [BottleNumber]
+  end
+
+  def self.register(candidate)
+    registry.prepend(candidate)
+  end
+
+  def self.inherited(candidate)
+    super
+    register(candidate)
+  end
+
+  def self.handles?(_number)
+    true
+  end
+
   attr_reader :number
 
   def initialize(number)
@@ -27,21 +46,8 @@ class BottleNumber
     "#{quantity} #{container}"
   end
 
-  def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    when 6
-      BottleNumber6
-    else
-      BottleNumber
-    end.new(number)
-  end
-
   def container
-    "bottles"
+    'bottles'
   end
 
   def quantity
@@ -49,11 +55,11 @@ class BottleNumber
   end
 
   def pronoun
-    "one"
+    'one'
   end
 
   def action
-    "Take #{pronoun} down and pass it around"      
+    "Take #{pronoun} down and pass it around"
   end
 
   def successor
@@ -62,12 +68,16 @@ class BottleNumber
 end
 
 class BottleNumber0 < BottleNumber
+  def self.handles?(number)
+    number.zero?
+  end
+
   def quantity
-    "no more"
+    'no more'
   end
 
   def action
-    "Go to the store and buy some more"
+    'Go to the store and buy some more'
   end
 
   def successor
@@ -75,23 +85,30 @@ class BottleNumber0 < BottleNumber
   end
 end
 
-
 class BottleNumber1 < BottleNumber
+  def self.handles?(number)
+    number == 1
+  end
+
   def container
-    "bottle"
+    'bottle'
   end
 
   def pronoun
-    "it"
+    'it'
   end
 end
 
 class BottleNumber6 < BottleNumber
+  def self.handles?(number)
+    number == 6
+  end
+
   def quantity
-    "1"
+    '1'
   end
 
   def container
-    "six-pack"
+    'six-pack'
   end
 end
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -1,90 +1,70 @@
-gem 'minitest', '~> 5.4'
-require 'minitest/autorun'
-require 'minitest/pride'
-require_relative '../lib/bottles'
+gem "minitest", "~> 5.4"
+require "minitest/autorun"
+require "minitest/pride"
+require_relative "../lib/bottles"
 
 class BottlesTest < Minitest::Test
   def test_the_first_verse
     expected =
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n"
+      "99 bottles of beer on the wall, 99 bottles of beer.\n"  +
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(99)
   end
 
   def test_another_verse
     # skip
     expected =
-      "3 bottles of beer on the wall, " +
-      "3 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of beer on the wall.\n"
+      "3 bottles of beer on the wall, 3 bottles of beer.\n"  +
+      "Take one down and pass it around, 2 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(3)
   end
 
   def test_verse_2
     # skip
     expected =
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\n"  +
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
   end
 
   def test_verse_1
     # skip
     expected =
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
-     assert_equal expected, Bottles.new.verse(1)
+      "1 bottle of beer on the wall, 1 bottle of beer.\n"  +
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(1)
   end
 
   def test_verse_0
     # skip
     expected =
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\n"  +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(0)
   end
 
   def test_a_couple_verses
     # skip
     expected =
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
+      "99 bottles of beer on the wall, 99 bottles of beer.\n"  +
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n" +
       "\n" +
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
+      "98 bottles of beer on the wall, 98 bottles of beer.\n" +
+      "Take one down and pass it around, 97 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verses(99, 98)
   end
 
   def test_a_few_verses
     # skip
     expected =
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n" +
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
       "\n" +
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n" +
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+      "Take it down and pass it around, no more bottles of beer on the wall.\n" +
       "\n" +
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verses(2, 0)
   end
 
